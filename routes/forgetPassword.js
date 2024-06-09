5c3e8c610e606ffe298c7b73b567d3f34ee7d868
akshdeepsingh <akshdeep@flick2know.com>
2024-06-10 01:12:26 +0530

    fix: forget password link change

--- routes/forgetPassword.js
+++ routes/forgetPassword.js
@@ -35,7 +35,7 @@ router.post('/request-password-reset', async (req, res) => {
       subject: 'Password Reset',
       text: `You are receiving this because you have requested the reset of the password for your account.\n\n
              Please click on the following link, or paste this into your browser to complete the process:\n\n
-             http://localhost:3001/forgetPassword?token=${token}\n\n
+             https://specter-chatbox-frontend-production.up.railway.app/forgetPassword?token=${token}\n\n
              If you did not request this, please ignore this email and your password will remain unchanged.\n`,
     };
 
